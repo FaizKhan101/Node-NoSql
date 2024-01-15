7d06376b22db8bcc47af41cfbfb80e070357086c
Faiz <faizkhan8225@gmail.com>
2024-01-15 11:42:23 +0530

    Create a user model, dummy user, and able to add product via user id

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const db = require("./util/database");
+const User = require("./models/user")
 
 const app = express();
 
@@ -17,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("65a4ca8683653f2aad228d0e")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
