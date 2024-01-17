c880b7076c003e9daa591ef8e7dafca672c13508
Faiz <faizkhan8225@gmail.com>
2024-01-17 12:05:35 +0530

    Create User Schema and add a dummy user

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-const app = express();
+const app = express()
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -17,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("65a4ca8683653f2aad228d0e")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("65a77489668cc473921f1f69")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +34,19 @@ app.use(errorController.get404);
 
 mongoose
   .connect("mongodb://localhost:27017/node-nosql")
+  .then((result) => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Faiz",
+          email: "test@test.com",
+          cart: { items: [] },
+        });
+        return user.save();
+      }
+      return;
+    })
+  })
   .then((result) => {
     app.listen(3000, () => {
       console.log("Server starts at port 3000!");
